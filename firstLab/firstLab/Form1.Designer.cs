1bc7d1688f105913d1dd8c5102687b9fd70082b6
Eenclaver <soshnevand@gmail.com>
2023-04-15 13:38:39 +0300

    Here it is

---
+++ firstLab/firstLab/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace firstLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_answer = new System.Windows.Forms.Button();
+            this.lb_triangle = new System.Windows.Forms.Label();
+            this.tb_firstTriangleX = new System.Windows.Forms.TextBox();
+            this.tb_secondTriangleX = new System.Windows.Forms.TextBox();
+            this.tb_thirdTriangleX = new System.Windows.Forms.TextBox();
+            this.lb_firstTriangle = new System.Windows.Forms.Label();
+            this.lb_secondTriangle = new System.Windows.Forms.Label();
+            this.lb_thirdTriangle = new System.Windows.Forms.Label();
+            this.lb_dot = new System.Windows.Forms.Label();
+            this.lb_xTriangle = new System.Windows.Forms.Label();
+            this.lb_yTriangle = new System.Windows.Forms.Label();
+            this.tb_thirdTriangleY = new System.Windows.Forms.TextBox();
+            this.tb_secondTriangleY = new System.Windows.Forms.TextBox();
+            this.tb_firstTriangleY = new System.Windows.Forms.TextBox();
+            this.tb_dotY = new System.Windows.Forms.TextBox();
+            this.lb_yDot = new System.Windows.Forms.Label();
+            this.lb_xDot = new System.Windows.Forms.Label();
+            this.tb_dotX = new System.Windows.Forms.TextBox();
+            this.lb_answer = new System.Windows.Forms.Label();
+            this.bt_upload = new System.Windows.Forms.Button();
+            this.bt_save = new System.Windows.Forms.Button();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // bt_answer
+            // 
+            this.bt_answer.Location = new System.Drawing.Point(651, 182);
+            this.bt_answer.Name = "bt_answer";
+            this.bt_answer.Size = new System.Drawing.Size(108, 43);
+            this.bt_answer.TabIndex = 0;
+            this.bt_answer.Text = "Рассчитать";
+            this.bt_answer.UseVisualStyleBackColor = true;
+            this.bt_answer.Click += new System.EventHandler(this.bt_answer_Click);
+            // 
+            // lb_triangle
+            // 
+            this.lb_triangle.AutoSize = true;
+            this.lb_triangle.Font = new System.Drawing.Font("Harlow Solid Italic", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_triangle.Location = new System.Drawing.Point(12, 20);
+            this.lb_triangle.Name = "lb_triangle";
+            this.lb_triangle.Size = new System.Drawing.Size(115, 22);
+            this.lb_triangle.TabIndex = 3;
+            this.lb_triangle.Text = "Треугольник";
+            // 
+            // tb_firstTriangleX
+            // 
+            this.tb_firstTriangleX.Location = new System.Drawing.Point(116, 77);
+            this.tb_firstTriangleX.Name = "tb_firstTriangleX";
+            this.tb_firstTriangleX.Size = new System.Drawing.Size(100, 22);
+            this.tb_firstTriangleX.TabIndex = 4;
+            this.tb_firstTriangleX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.inputMask);
+            this.tb_firstTriangleX.Leave += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // tb_secondTriangleX
+            // 
+            this.tb_secondTriangleX.Location = new System.Drawing.Point(116, 105);
+            this.tb_secondTriangleX.Name = "tb_secondTriangleX";
+            this.tb_secondTriangleX.Size = new System.Drawing.Size(100, 22);
+            this.tb_secondTriangleX.TabIndex = 5;
+            this.tb_secondTriangleX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.inputMask);
+            this.tb_secondTriangleX.Leave += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // tb_thirdTriangleX
+            // 
+            this.tb_thirdTriangleX.Location = new System.Drawing.Point(116, 133);
+            this.tb_thirdTriangleX.Name = "tb_thirdTriangleX";
+            this.tb_thirdTriangleX.Size = new System.Drawing.Size(100, 22);
+            this.tb_thirdTriangleX.TabIndex = 6;
+            this.tb_thirdTriangleX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.inputMask);
+            this.tb_thirdTriangleX.Leave += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // lb_firstTriangle
+            // 
+            this.lb_firstTriangle.AutoSize = true;
+            this.lb_firstTriangle.Location = new System.Drawing.Point(13, 77);
+            this.lb_firstTriangle.Name = "lb_firstTriangle";
+            this.lb_firstTriangle.Size = new System.Drawing.Size(97, 16);
+            this.lb_firstTriangle.TabIndex = 7;
+            this.lb_firstTriangle.Text = "Первая точка";
+            // 
+            // lb_secondTriangle
+            // 
+            this.lb_secondTriangle.AutoSize = true;
+            this.lb_secondTriangle.Location = new System.Drawing.Point(13, 105);
+            this.lb_secondTriangle.Name = "lb_secondTriangle";
+            this.lb_secondTriangle.Size = new System.Drawing.Size(95, 16);
+            this.lb_secondTriangle.TabIndex = 8;
+            this.lb_secondTriangle.Text = "Вторая точка";
+            // 
+            // lb_thirdTriangle
+            // 
+            this.lb_thirdTriangle.AutoSize = true;
+            this.lb_thirdTriangle.Location = new System.Drawing.Point(13, 133);
+            this.lb_thirdTriangle.Name = "lb_thirdTriangle";
+            this.lb_thirdTriangle.Size = new System.Drawing.Size(94, 16);
+            this.lb_thirdTriangle.TabIndex = 9;
+            this.lb_thirdTriangle.Text = "Третья точка";
+            // 
+            // lb_dot
+            // 
+            this.lb_dot.AutoSize = true;
+            this.lb_dot.Font = new System.Drawing.Font("Harlow Solid Italic", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_dot.Location = new System.Drawing.Point(416, 20);
+            this.lb_dot.Name = "lb_dot";
+            this.lb_dot.Size = new System.Drawing.Size(59, 22);
+            this.lb_dot.TabIndex = 10;
+            this.lb_dot.Text = "Точка";
+            // 
+            // lb_xTriangle
+            // 
+            this.lb_xTriangle.AutoSize = true;
+            this.lb_xTriangle.Font = new System.Drawing.Font("Agency FB", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_xTriangle.Location = new System.Drawing.Point(158, 43);
+            this.lb_xTriangle.Name = "lb_xTriangle";
+            this.lb_xTriangle.Size = new System.Drawing.Size(18, 21);
+            this.lb_xTriangle.TabIndex = 11;
+            this.lb_xTriangle.Text = "X";
+            // 
+            // lb_yTriangle
+            // 
+            this.lb_yTriangle.AutoSize = true;
+            this.lb_yTriangle.Font = new System.Drawing.Font("Agency FB", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_yTriangle.Location = new System.Drawing.Point(294, 43);
+            this.lb_yTriangle.Name = "lb_yTriangle";
+            this.lb_yTriangle.Size = new System.Drawing.Size(19, 21);
+            this.lb_yTriangle.TabIndex = 12;
+            this.lb_yTriangle.Text = "Y";
+            // 
+            // tb_thirdTriangleY
+            // 
+            this.tb_thirdTriangleY.Location = new System.Drawing.Point(252, 133);
+            this.tb_thirdTriangleY.Name = "tb_thirdTriangleY";
+            this.tb_thirdTriangleY.Size = new System.Drawing.Size(100, 22);
+            this.tb_thirdTriangleY.TabIndex = 15;
+            this.tb_thirdTriangleY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.inputMask);
+            this.tb_thirdTriangleY.Leave += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // tb_secondTriangleY
+            // 
+            this.tb_secondTriangleY.Location = new System.Drawing.Point(252, 105);
+            this.tb_secondTriangleY.Name = "tb_secondTriangleY";
+            this.tb_secondTriangleY.Size = new System.Drawing.Size(100, 22);
+            this.tb_secondTriangleY.TabIndex = 14;
+            this.tb_secondTriangleY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.inputMask);
+            this.tb_secondTriangleY.Leave += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // tb_firstTriangleY
+            // 
+            this.tb_firstTriangleY.Location = new System.Drawing.Point(252, 77);
+            this.tb_firstTriangleY.Name = "tb_firstTriangleY";
+            this.tb_firstTriangleY.Size = new System.Drawing.Size(100, 22);
+            this.tb_firstTriangleY.TabIndex = 13;
+            this.tb_firstTriangleY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.inputMask);
+            this.tb_firstTriangleY.Leave += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // tb_dotY
+            // 
+            this.tb_dotY.Location = new System.Drawing.Point(605, 77);
+            this.tb_dotY.Name = "tb_dotY";
+            this.tb_dotY.Size = new System.Drawing.Size(100, 22);
+            this.tb_dotY.TabIndex = 19;
+            this.tb_dotY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.inputMask);
+            this.tb_dotY.Leave += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // lb_yDot
+            // 
+            this.lb_yDot.AutoSize = true;
+            this.lb_yDot.Font = new System.Drawing.Font("Agency FB", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_yDot.Location = new System.Drawing.Point(647, 43);
+            this.lb_yDot.Name = "lb_yDot";
+            this.lb_yDot.Size = new System.Drawing.Size(19, 21);
+            this.lb_yDot.TabIndex = 18;
+            this.lb_yDot.Text = "Y";
+            // 
+            // lb_xDot
+            // 
+            this.lb_xDot.AutoSize = true;
+            this.lb_xDot.Font = new System.Drawing.Font("Agency FB", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_xDot.Location = new System.Drawing.Point(511, 43);
+            this.lb_xDot.Name = "lb_xDot";
+            this.lb_xDot.Size = new System.Drawing.Size(18, 21);
+            this.lb_xDot.TabIndex = 17;
+            this.lb_xDot.Text = "X";
+            // 
+            // tb_dotX
+            // 
+            this.tb_dotX.Location = new System.Drawing.Point(469, 77);
+            this.tb_dotX.Name = "tb_dotX";
+            this.tb_dotX.Size = new System.Drawing.Size(100, 22);
+            this.tb_dotX.TabIndex = 16;
+            this.tb_dotX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.inputMask);
+            this.tb_dotX.Leave += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // lb_answer
+            // 
+            this.lb_answer.AutoSize = true;
+            this.lb_answer.Location = new System.Drawing.Point(420, 110);
+            this.lb_answer.Name = "lb_answer";
+            this.lb_answer.Size = new System.Drawing.Size(0, 16);
+            this.lb_answer.TabIndex = 20;
+            this.lb_answer.Visible = false;
+            // 
+            // bt_upload
+            // 
+            this.bt_upload.Location = new System.Drawing.Point(12, 172);
+            this.bt_upload.Name = "bt_upload";
+            this.bt_upload.Size = new System.Drawing.Size(108, 53);
+            this.bt_upload.TabIndex = 21;
+            this.bt_upload.Text = "Загрузить данные";
+            this.bt_upload.UseVisualStyleBackColor = true;
+            this.bt_upload.Click += new System.EventHandler(this.bt_upload_Click);
+            // 
+            // bt_save
+            // 
+            this.bt_save.Location = new System.Drawing.Point(126, 172);
+            this.bt_save.Name = "bt_save";
+            this.bt_save.Size = new System.Drawing.Size(108, 53);
+            this.bt_save.TabIndex = 22;
+            this.bt_save.Text = "Сохранить данные";
+            this.bt_save.UseVisualStyleBackColor = true;
+            this.bt_save.Click += new System.EventHandler(this.bt_save_Click);
+            // 
+            // saveFile
+            // 
+            this.saveFile.DefaultExt = "txt";
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 237);
+            this.Controls.Add(this.bt_save);
+            this.Controls.Add(this.bt_upload);
+            this.Controls.Add(this.lb_answer);
+            this.Controls.Add(this.tb_dotY);
+            this.Controls.Add(this.lb_yDot);
+            this.Controls.Add(this.lb_xDot);
+            this.Controls.Add(this.tb_dotX);
+            this.Controls.Add(this.tb_thirdTriangleY);
+            this.Controls.Add(this.tb_secondTriangleY);
+            this.Controls.Add(this.tb_firstTriangleY);
+            this.Controls.Add(this.lb_yTriangle);
+            this.Controls.Add(this.lb_xTriangle);
+            this.Controls.Add(this.lb_dot);
+            this.Controls.Add(this.lb_thirdTriangle);
+            this.Controls.Add(this.lb_secondTriangle);
+            this.Controls.Add(this.lb_firstTriangle);
+            this.Controls.Add(this.tb_thirdTriangleX);
+            this.Controls.Add(this.tb_secondTriangleX);
+            this.Controls.Add(this.tb_firstTriangleX);
+            this.Controls.Add(this.lb_triangle);
+            this.Controls.Add(this.bt_answer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_answer;
+        private System.Windows.Forms.Label lb_triangle;
+        private System.Windows.Forms.TextBox tb_firstTriangleX;
+        private System.Windows.Forms.TextBox tb_secondTriangleX;
+        private System.Windows.Forms.TextBox tb_thirdTriangleX;
+        private System.Windows.Forms.Label lb_firstTriangle;
+        private System.Windows.Forms.Label lb_secondTriangle;
+        private System.Windows.Forms.Label lb_thirdTriangle;
+        private System.Windows.Forms.Label lb_dot;
+        private System.Windows.Forms.Label lb_xTriangle;
+        private System.Windows.Forms.Label lb_yTriangle;
+        private System.Windows.Forms.TextBox tb_thirdTriangleY;
+        private System.Windows.Forms.TextBox tb_secondTriangleY;
+        private System.Windows.Forms.TextBox tb_firstTriangleY;
+        private System.Windows.Forms.TextBox tb_dotY;
+        private System.Windows.Forms.Label lb_yDot;
+        private System.Windows.Forms.Label lb_xDot;
+        private System.Windows.Forms.TextBox tb_dotX;
+        private System.Windows.Forms.Label lb_answer;
+        private System.Windows.Forms.Button bt_upload;
+        private System.Windows.Forms.Button bt_save;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.OpenFileDialog openFile;
+    }
+}
+
---
+++ firstLab/firstLab/Form1.cs
@@ -0,0 +1,120 @@
+﻿using firstLab.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace firstLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Shown(Object sender, EventArgs e)
+        {
+            MessageBox.Show("Для заданной точки и треугольника на плоскости определить, принадлежит ли точка треугольнику.", "Сошнев Андрей 415а, работа номер 1, вариант 6", MessageBoxButtons.OK);
+        }
+
+        private void inputMask(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar <= 43 || e.KeyChar >= 58) && e.KeyChar != 8)
+                e.Handled = true;
+            if (e.KeyChar == 47) e.Handled = true;
+            if (e.KeyChar == 46) e.Handled = true;
+        }
+
+
+        private void bt_answer_Click(object sender, EventArgs e)
+        {
+            double a = (Convert.ToDouble(tb_firstTriangleX.Text) - Convert.ToDouble(tb_dotX.Text)) * (Convert.ToDouble(tb_secondTriangleY.Text) - Convert.ToDouble(tb_firstTriangleY.Text)) - (Convert.ToDouble(tb_secondTriangleX.Text) - Convert.ToDouble(tb_firstTriangleX.Text)) * (Convert.ToDouble(tb_firstTriangleY.Text) - Convert.ToDouble(tb_dotY.Text));
+            double b = (Convert.ToDouble(tb_secondTriangleX.Text) - Convert.ToDouble(tb_dotX.Text)) * (Convert.ToDouble(tb_thirdTriangleY.Text) - Convert.ToDouble(tb_secondTriangleY.Text)) - (Convert.ToDouble(tb_thirdTriangleX.Text) - Convert.ToDouble(tb_secondTriangleX.Text)) * (Convert.ToDouble(tb_secondTriangleY.Text) - Convert.ToDouble(tb_dotX.Text));
+            double c = (Convert.ToDouble(tb_thirdTriangleX.Text) - Convert.ToDouble(tb_dotX.Text)) * (Convert.ToDouble(tb_firstTriangleY.Text) - Convert.ToDouble(tb_thirdTriangleY.Text)) - (Convert.ToDouble(tb_firstTriangleX.Text) - Convert.ToDouble(tb_thirdTriangleX.Text)) * (Convert.ToDouble(tb_thirdTriangleY.Text) - Convert.ToDouble(tb_dotY.Text));
+
+            lb_answer.Visible = true;
+
+            if ((a >= 0 && b >= 0 && c >= 0) || (a <= 0 && b <= 0 && c <= 0))
+            {
+          
+                lb_answer.Text="Принадлежит треугольнику";
+            }
+            else
+            {
+                lb_answer.Text = "Не принадлежит треугольнику";
+            }
+        }
+
+        private void tb_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Windows.Forms.TextBox tb = (System.Windows.Forms.TextBox)sender;
+                double num = Convert.ToDouble(tb.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Пожалуйста, введите верные данные", "Ахтунг!Ошибка", MessageBoxButtons.OK);
+                System.Windows.Forms.TextBox tb = (System.Windows.Forms.TextBox)sender;
+                tb.Clear();
+            }
+        }
+
+        private void bt_upload_Click(object sender, EventArgs e)
+        {
+            if (openFile.ShowDialog() == DialogResult.Cancel)
+                return;
+            double filedata;
+            // получаем выбранный файл
+            string filename = openFile.FileName;
+            // читаем файл в строку
+            string[] fileText = System.IO.File.ReadAllLines(filename);
+            try
+            {
+                if (fileText.Length > 8) { double bonk = Convert.ToDouble("Bonk!"); }
+                for (int i = 0;i < fileText.Length; i++){
+                    filedata = Convert.ToDouble(fileText[i]);
+                }
+                tb_dotX.Text = fileText[0];
+                tb_dotY.Text = fileText[1];
+                tb_firstTriangleX.Text = fileText[2];
+                tb_firstTriangleY.Text = fileText[3];
+                tb_secondTriangleX.Text = fileText[4];
+                tb_secondTriangleY.Text = fileText[5];
+                tb_thirdTriangleX.Text = fileText[6];
+                tb_thirdTriangleY.Text = fileText[7];
+                MessageBox.Show("Файл открыт");
+            }
+            catch
+            {
+                MessageBox.Show("Пожалуйста, введите верные данные", "Ахтунг!Ошибка", MessageBoxButtons.OK);
+            }
+        }
+
+        private void bt_save_Click(object sender, EventArgs e)
+        {
+            if (saveFile.ShowDialog() == DialogResult.Cancel)
+                return;
+            string[] filedata=new string[8];
+            filedata[0]= tb_dotX.Text;
+            filedata[1]= tb_dotY.Text;
+            filedata[2]= tb_firstTriangleX.Text;
+            filedata[3]= tb_firstTriangleY.Text;
+            filedata[4]= tb_secondTriangleX.Text;
+            filedata[5]= tb_secondTriangleY.Text;
+            filedata[6]= tb_thirdTriangleX.Text;
+            filedata[7]= tb_thirdTriangleY.Text;
+            // получаем выбранный файл
+            string filename = saveFile.FileName;
+            // сохраняем текст в файл
+            System.IO.File.WriteAllLines(filename, filedata);
+            MessageBox.Show("Файл сохранен");
+        }
+    }
+}
